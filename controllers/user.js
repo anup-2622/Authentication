f9db630aabf5248040c9c2d47dcfcfe9175fc63e
Anup <anupisri107@gmail.com>
2024-02-07 00:16:42 +0530

    SignUp is Done

--- controllers/user.js
+++ controllers/user.js
@@ -1,17 +1,32 @@
 
 const User = require("../models/user")
 
-async function handleUserSignUp(req , res){
-    const {name , email , password} = req.body;
-    await User.create({
-        name,
-        email,
-        password
-    })
-    res.status(201).json({message:"User Created Sucessfully " })
+
+exports.signup = async (req, res)=>{
+    try{
+        const newUser = await User.create({
+            name:req.body.name,
+            email:req.body.email,
+            password:req.body.password
+        })
+        res.status(201).json({message:'user Created successfully' , user: newUser}  )
+    }catch(e){
+        res.status(500).json({error:e.message})
+    }
 }
 
 
-module.exports={
-    handleUserSignUp,
-}
+// async function handleUserSignUp(req , res){
+//     const {name , email , password} = req.body;
+//     await User.create({
+//         name,
+//         email,
+//         password
+//     })
+//     res.status(201).json({message:"User Created Sucessfully " })
+// }
+
+
+// module.exports={
+//     handleUserSignUp
+// }
--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRoute = require("./routes/user")
 const app = express();
+const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+const db = require("./db/db")
 
-app.get("/",(req, res)=>{
-     res.send("happy coding");
-})
 
-app.use("/user" ,userRoute)
+// mongoose.connect('mongodb://localhost:27017/auth_example', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// // const db = mongoose.connection;
+
+
+// app.get("/",(req, res)=>{
+//      res.send("happy coding");
+// })
+
+app.use(bodyParser.json());
+
+app.use('/user', userRoute);
 // app.render("<p>Happy Coding</p>")
 
 app.listen(9000 , ()=>{
